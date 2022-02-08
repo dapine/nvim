ea787f0be9e971beba0e6eeafa149951ed711742
Breno Pinheiro <davepinh@gmail.com>
2022-02-08 19:40:51 -0300

    themes updated

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,9 +1,16 @@
 local M = {}
 
-cur_hour = os.date("*t", os.time()).hour
+local cur_hour = os.date("*t", os.time()).hour
+local is_night = cur_hour >= 19
 
-M.background = cur_hour >= 19 and "dark" or "light"
-M.colorscheme = "gruvbox"
-M.lualine_theme = cur_hour >= 19 and "gruvbox_dark" or "gruvbox_light" 
+colorscheme_light = "modus-operandi"
+colorscheme_dark = "modus-vivendi"
+
+lualine_light = "onelight"
+lualine_dark = "modus-vivendi"
+
+M.background = is_night and "dark" or "light"
+M.colorscheme = is_night and colorscheme_dark or colorscheme_light
+M.lualine_theme = is_night and lualine_dark or lualine_light
 
 return M
