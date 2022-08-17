b1357af5c123019c45a6eea08dfc26e76ed7753a
Breno Pinheiro <davepinh@gmail.com>
2022-08-17 15:07:22 -0300

    Remove `lua/goimport.lua` and put it in ftplugin

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -1,6 +1,22 @@
+-- https://cs.opensource.google/go/x/tools/+/refs/tags/gopls/v0.8.3:gopls/doc/vim.md#neovim-imports
+function org_imports(wait_ms)
+	local params = vim.lsp.util.make_range_params()
+	params.context = {only = {"source.organizeImports"}}
+	local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+	for _, res in pairs(result or {}) do
+		for _, r in pairs(res.result or {}) do
+			if r.edit then
+				vim.lsp.util.apply_workspace_edit(r.edit, "utf-8")
+			else
+				vim.lsp.buf.execute_command(r.command)
+			end
+		end
+	end
+end
+
 vim.api.nvim_create_autocmd('BufWritePre', {
 	pattern = { "*.go" },
-	command = 'lua local goimport = require "goimport"; goimport.org_imports(1000)',
+	command = 'lua org_imports(1000)',
 })
 
 vim.api.nvim_create_autocmd('BufWritePre', {
