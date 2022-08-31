72fc2a9d9ca749e1420f03607c3f9e7376a321e7
Breno Pinheiro <davepinh@gmail.com>
2022-08-31 15:02:46 -0300

    Add commands to `:make` with cargo

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -2,3 +2,16 @@ vim.api.nvim_create_autocmd('BufWritePre', {
 	pattern = { "*.rs" },
 	command = 'lua vim.lsp.buf.formatting_sync(nil, 1000)',
 })
+
+function cargo_run()
+	vim.cmd("set makeprg=cargo\\ run\\ --message-format=short")
+	vim.cmd("make")
+end
+
+function cargo_build()
+	vim.cmd("set makeprg=cargo\\ build\\ --message-format=short")
+	vim.cmd("make")
+end
+
+vim.api.nvim_create_user_command("CargoRun", cargo_run, {})
+vim.api.nvim_create_user_command("CargoBuild", cargo_build, {})
