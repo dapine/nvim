6ab7096788d98b2044bda76bf1c69b49eb813509
Breno Pinheiro <davepinh@gmail.com>
2022-09-08 13:15:24 -0300

    Add packer bootstrap

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,6 +1,21 @@
-vim.cmd [[packadd packer.nvim]]
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({'git',
+							 'clone',
+							 '--depth',
+							 '1',
+							 'https://github.com/wbthomason/packer.nvim', install_path})
+		vim.cmd [[packadd packer.nvim]]
+		return true
+	end
+	return false
+end
 
-return require('packer').startup(function()
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
 	-- essential
   use 'wbthomason/packer.nvim'
   use 'tpope/vim-commentary'
@@ -32,4 +47,8 @@ return require('packer').startup(function()
   }
 	use 'williamboman/nvim-lsp-installer'
 	use { "mfussenegger/nvim-jdtls", ft = { "java" } }
+
+	if packer_bootstrap then
+		require('packer').sync()
+	end
 end)
