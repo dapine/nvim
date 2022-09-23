976a3fe04c077bc7caf604aab9c21969a4858123
Breno Pinheiro <davepinh@gmail.com>
2022-09-23 09:49:34 -0300

    Add `venv` directory to telescope ignore patterns

--- after/plugin/config/telescope.lua
+++ after/plugin/config/telescope.lua
@@ -7,6 +7,6 @@ end
 
 require('telescope').setup{
   defaults = {
-    file_ignore_patterns = { "node_modules", "_build", "deps", "target" }
+    file_ignore_patterns = { "node_modules", "_build", "deps", "target", "venv" }
   }
 }
