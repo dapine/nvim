eb7f83b6a6e6df495f28214fa122a46d986b7ea8
Breno Pinheiro <davepinh@gmail.com>
2023-04-04 11:06:09 -0300

    Update telescope settings

--- after/plugin/config/telescope.lua
+++ after/plugin/config/telescope.lua
@@ -7,7 +7,16 @@ end
 
 require('telescope').setup{
   defaults = {
-    file_ignore_patterns = { "node_modules", "_build", "deps", "target", "venv" }
-  }
+    file_ignore_patterns = { "node_modules", "_build", "deps", "target", "venv" },
+  },
+	extensions = {
+		file_browser = {
+			cwd_to_path = true,
+			dir_icon = "",
+			prompt_path = true,
+			initial_mode = "normal",
+			display_stat = {}
+		}
+	},
 }
 require("telescope").load_extension "file_browser"
