d91d192b33e8317a7f45e39215a254bd7d63be57
Breno Pinheiro <davepinh@gmail.com>
2023-04-04 13:03:45 -0300

    Use mason-lspconfig auto server setup instead of rolling my own

--- after/plugin/config/mason.lua
+++ after/plugin/config/mason.lua
@@ -13,3 +13,39 @@ end
 
 require("mason").setup {}
 require("mason-lspconfig").setup {}
+
+require("mason-lspconfig").setup_handlers {
+	function (server_name)
+		require("lspconfig")[server_name].setup {}
+	end,
+}
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<leader>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', '<leader>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+    vim.keymap.set('n', '<leader>so', require('telescope.builtin').lsp_document_symbols, opts)
+		vim.api.nvim_create_user_command('Format', function() vim.lsp.buf.format { async = true } end, {})
+		vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+		vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+  end,
+})
