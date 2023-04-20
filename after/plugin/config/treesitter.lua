1cc7c8874e8c5ba985547f996509e2e1efbae67d
Breno Pinheiro <davepinh@gmail.com>
2023-04-20 14:56:00 -0300

    Make some grammars ensure_installed

--- after/plugin/config/treesitter.lua
+++ after/plugin/config/treesitter.lua
@@ -6,6 +6,10 @@ if not ok then
 end
 
 require'nvim-treesitter.configs'.setup {
+	ensure_installed = { "rust", "lua", "javascript", "typescript", 
+											 "python", "bash", "html", "css",
+										 	 "elixir", "java", "json", "toml",
+										 	 "yaml" },
   highlight = {
     enable = true,
   }
