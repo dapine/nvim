2b26be2515e1e36c51a8ed3c4228ef024c869fa8
Breno Pinheiro <davepinh@gmail.com>
2023-06-21 10:00:06 -0300

    Add java checkstyle

--- after/plugin/config/null-ls.lua
+++ after/plugin/config/null-ls.lua
@@ -1,9 +1,13 @@
 local null_ls = require("null-ls")
 
 local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
   sources = {
+    diagnostics.checkstyle.with({
+      extra_args = { "-c", "/google_checks.xml" },
+    }),
     formatting.google_java_format,
   }
 })
