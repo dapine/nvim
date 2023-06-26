45421a3efad2d8d01287a16e08bdffd4c23482f9
Breno Pinheiro <davepinh@gmail.com>
2023-06-26 14:45:49 -0300

    Update gitsigns configs

--- after/plugin/config/gitsigns.lua
+++ after/plugin/config/gitsigns.lua
@@ -1,1 +1,8 @@
-require('gitsigns').setup()
+require('gitsigns').setup({
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+    local map = vim.keymap.set
+
+    map('n', '<leader>gb', gs.toggle_current_line_blame)
+  end
+})
