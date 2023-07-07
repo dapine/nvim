e08b6f84dd66619c50d8139b35723b5666b434e6
Breno Pinheiro <davepinh@gmail.com>
2023-07-07 08:59:28 -0300

    Move LSP keymaps to `maps.lua`

--- lua/maps.lua
+++ lua/maps.lua
@@ -62,8 +62,32 @@ map({"n", "o", "x"}, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc =
 map({"n", "o", "x"}, "ge", "<cmd>lua require('spider').motion('ge')<CR>", { desc = "Spider-ge" })
 
 vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('LspKeymaps', {}),
   callback = function(args)
+    vim.bo[args.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
     local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+    local opts = { buffer = args.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<leader>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', '<leader>so', require('telescope.builtin').lsp_document_symbols, opts)
+		vim.api.nvim_create_user_command('Format', function() vim.lsp.buf.format { async = true } end, {})
+		vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+		vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+    vim.keymap.set('n', '<leader>d', require('telescope.builtin').diagnostics, opts)
+
     if client.server_capabilities.signatureHelpProvider then
       map('n', '<Leader>s', vim.lsp.buf.signature_help, options)
     end
