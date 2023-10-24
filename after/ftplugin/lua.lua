4950b65dbc91aa942de917e35756a6de0cb27ba3
Breno Pinheiro <davepinh@gmail.com>
2023-10-24 11:40:40 -0300

    Update lua lsp config

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,9 +1,30 @@
+-- copied from nvim-lspconfig
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#lua_ls
+
 require('lspconfig').lua_ls.setup({
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = {'vim'}
-			}
-		}
-	}
+  on_init = function(client)
+    local path = client.workspace_folders[1].name
+    if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+      client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+        Lua = {
+          runtime = {
+            version = 'LuaJIT'
+          },
+          workspace = {
+            checkThirdParty = false,
+            library = {
+              vim.env.VIMRUNTIME
+            }
+          },
+          diagnostics = {
+            globals = { 'vim' }
+          }
+
+        }
+      })
+
+      client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+    end
+    return true
+  end
 })
