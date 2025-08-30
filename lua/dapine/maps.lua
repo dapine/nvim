f47ab8e975f2d3a6a8f7884cd2289095ebea75ef
Breno Pinheiro <davepinh@gmail.com>
2025-08-30 20:28:34 -0300

    Remove unused/redundant keymaps

--- lua/dapine/maps.lua
+++ lua/dapine/maps.lua
@@ -1,30 +1,15 @@
 local map = vim.keymap.set
 
-local function close()
-	local buftype = vim.api.nvim_buf_get_option(0, "buftype")
-	if buftype == "terminal" then
-		vim.cmd("bw!")
-	else
-		vim.cmd("bw")
-	end
-end
-
 map("n", "<Space>", "", {})
 vim.g.mapleader = " "
 
-local options = { noremap = true }
+map("n", "0", "^")
+map("n", ",", ";")
+map("n", ";", ":")
+map("n", "s", "*")
 
-map("n", "0", "^", options)
-map("n", ",", ";", options)
-map("n", ";", ":", options)
-map("n", "s", "*", options)
-map("n", "j", "gj", options)
-map("n", "k", "gk", options)
-map("n", "n", "nzz", options)
-map("n", "N", "Nzz", options)
-
-map("i", "jk", "<esc>", options)
-map("i", "<C-d>", "<del>", options)
+map("i", "jk", "<esc>")
+map("i", "<C-d>", "<del>")
 
 vim.keymap.set({ "n", "x" }, "j", function()
 	return vim.v.count > 0 and "j" or "gj"
@@ -33,14 +18,8 @@ vim.keymap.set({ "n", "x" }, "k", function()
 	return vim.v.count > 0 and "k" or "gk"
 end, { noremap = true, expr = true })
 
-map("n", "<Leader>q", close, options)
+map({ "n", "v" }, "-", "<C-f>", { noremap = true })
+map({ "n", "v" }, "=", "<C-b>", { noremap = true })
 
-map("n", "<Leader>j", "<C-f>zz", options)
-map("n", "<Leader>k", "<C-b>zz", options)
-map("n", "-", "<C-f>zz", options)
-map("n", "=", "<C-b>zz", options)
-map("v", "-", "<C-f>zz", options)
-map("v", "=", "<C-b>zz", options)
-
-map("n", "<BS>", "<C-o>", options)
-map("n", "<A-BS>", "<C-i>", options)
+map("n", "<BS>", "<C-o>")
+map("n", "<A-BS>", "<C-i>")
