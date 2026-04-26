36a51398bbb471581ab6a6bd57304b536b3344e7
Breno Pinheiro <davepinh@gmail.com>
2026-04-25 22:44:07 -0300

    Add auto-dark-mode

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -8,6 +8,7 @@ vim.pack.add({ "https://github.com/nvim-lua/plenary.nvim.git" })
 vim.pack.add({ { src = "https://github.com/nvim-treesitter/nvim-treesitter.git", version = "master" } })
 vim.pack.add({ "https://github.com/RRethy/nvim-treesitter-endwise.git" })
 vim.pack.add({ "https://github.com/neovim/nvim-lspconfig" })
+vim.pack.add({ "https://github.com/f-person/auto-dark-mode.nvim" })
 
 require("gitsigns").setup({
     on_attach = function()
@@ -46,4 +47,26 @@ require("nvim-treesitter.config").setup({
     indent = { enable = true },
     endwise = { enable = true },
     ensure_installed = { "rust", "lua", "bash", "elixir", "eex", "heex" },
+})
+
+require("auto-dark-mode").setup({
+    set_dark_mode = function()
+        vim.api.nvim_set_option_value("background", "dark", {})
+        vim.cmd("colorscheme jellybeans-muted")
+
+        require("statusline").setup({
+            use_nerd_fonts = true,
+            colorscheme = "jellybeans"
+        })
+    end,
+    set_light_mode = function()
+        vim.api.nvim_set_option_value("background", "light", {})
+        vim.cmd("colorscheme modus_operandi")
+        require("statusline").setup({
+            use_nerd_fonts = true,
+            colorscheme = "modus_operandi"
+        })
+    end,
+    update_interval = 3000,
+    fallback = "light"
 })
